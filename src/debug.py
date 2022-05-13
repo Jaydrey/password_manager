5ec7e8f0e0e636a58a4c44f4ac74d3dadb4d1103
unknown <jaribwetshi64@gmail.com>
2022-05-13 13:22:17 +0300

    added file to generate random passwords

--- src/debug.py
+++ src/debug.py
@@ -1,2 +1,5 @@
-a = "doremifasolatido"
-print(len(a)*"*")
+import string
+import random
+
+a = [x for x in string.punctuation+string.ascii_letters+string.digits]
+
--- src/generate_password.py
+++ src/generate_password.py
@@ -1,2 +1,8 @@
 import string
 import random
+
+def generate()->str:
+    word_pool = string.ascii_letters+string.digits+string.punctuation
+    random.shuffle(word_pool)
+    weights = [random.randint(1, 30) for _ in range(len(word_pool))]
+    return "".join(random.choices(word_pool, k=15, weights=weights))
